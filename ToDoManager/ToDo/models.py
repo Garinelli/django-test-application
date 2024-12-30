d2e9461b883d9a41b7ed3b782928088205641467
Yuri <yura.voskanyan.2003@mail.ru>
2024-12-30 13:46:55 +0700

    Try 2..

--- ToDoManager/ToDo/models.py
+++ ToDoManager/ToDo/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.TimeField(auto_now_add=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
